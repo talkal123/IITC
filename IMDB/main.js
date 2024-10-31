2ecd112968270eac249e6a9d405f8b37497c5a04
talkal123 <talkal153@gmail.com>
2024-10-31 13:32:49 +0200

    ss

--- IMDB/main.js
+++ IMDB/main.js
@@ -15,6 +15,16 @@ const countNum = document.querySelector("#count");
 const searchInput = document.querySelector("#search");
 const searchBtn = document.querySelector("#Search-logo");
 const searchResult = document.querySelector("#search-result");
+const popularMovies = document.querySelector("#The-popular-Movies");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const upcomingMovies = document.querySelector("#Upcoming-Movies");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const RatedMovies = document.querySelector("#Top-Rated-Movies");
+const button5 = document.querySelector("#button5");
+const button6 = document.querySelector("#button6");
+
 
 
 const fMovies = [];
@@ -355,19 +365,6 @@ function SearchFunc() {
 }
 
 searchBtn.addEventListener("click", SearchFunc);
-
-
-
-const popularMovies = document.querySelector("#The-popular-Movies");
-const button1 = document.querySelector("#button1");
-const button2 = document.querySelector("#button2");
-const upcomingMovies = document.querySelector("#Upcoming-Movies");
-const button3 = document.querySelector("#button3");
-const button4 = document.querySelector("#button4");
-const RatedMovies = document.querySelector("#Top-Rated-Movies");
-const button5 = document.querySelector("#button5");
-const button6 = document.querySelector("#button6");
-
 
 
 
