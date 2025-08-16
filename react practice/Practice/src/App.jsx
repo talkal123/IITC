3ae439062f20d074c03a2206ba644a1722c2dba5
talkal123 <talkal153@gmail.com>
2025-08-16 13:10:16 +0300

    ss

--- react practice/Practice/src/App.jsx
+++ react practice/Practice/src/App.jsx
@@ -2,35 +2,43 @@ import { useEffect, useState } from "react";
 
 function App() {
 
-  
-  const arrays = [7, 2, 9, 4]
 
-
-  const theArray = arrays.reduce((total, item) =>  total + item );
-
-console.log(theArray);
+const [second,setSecond] = useState(0)
+const [minute,setMinute] = useState(0)
+const [hour,setHour] = useState(0)
 
 
 
-  
-  
 
+const upSecond = () => {setSecond(second + 1)}
+const upMinute = () => {setMinute(minute + 1)}
+const upHour = () => {setHour(hour + 1)}
+
+
+setTimeout(() => {
+  upSecond()
+}, 1000);
+
+setTimeout(() => {
+  upMinute()
+}, 60000);
+setTimeout(() => {
+  upHour()
+}, 600000);
+
+
+
+
+const [timer,setTimer] = useState(second + "" + minute + "" + hour)
 
 
   return (
     <>
       <div>
-        <h1>Products</h1>
+        <h1>User</h1>
         <div>
-          {/* <div>
-            {filterArr.map((item, idx) => (
-          <div key={idx}>
-            <p>{item.name} - ${item.price} - {item.inStock === true ? "True" : "False"}</p>
-          </div>
-        ))}
-      </div>
-          <button onClick={sort}>Sort</button> */}
-        </div>
+          <p>{timer}</p>
+         </div>
       </div>
     </>
   );
