0a4946a891c76e88b2e0c4e935dfbacfe8f06806
talkal123 <talkal153@gmail.com>
2025-08-23 19:49:47 +0300

    ss

--- FullStack Practice/react practice/Practice/src/App.jsx
+++ FullStack Practice/react practice/Practice/src/App.jsx
@@ -8,14 +8,53 @@ function App() {
 // const result = newWord.join("")
 // console.log(result);
 
-const arr = [1,2,3,4]
+// const arr = [1,2,3,4]
 
-const sum = arr.reduce((total,item) => total + item)
-console.log(sum);
-
-
+// const sum = arr.reduce((total,item) => total + item)
+// console.log(sum);
 
 
+// const words = ["a","b","a","c","a"]
+
+// let count = {}
+
+// words.forEach(w => {
+//   if (w !== "") {
+//     count[w] = (count[w] || 0) + 1
+//   }
+// })
+
+// console.log(count)
+
+
+const arr = [1,4,7]
+const arr2 = [2,3,6]
+
+
+const newArr = [...arr, ...arr2]
+const sortedArr = newArr.sort((a,b) => a - b)
+
+console.log(sortedArr);
+
+
+// const arr = [1,2,2,3,4,4,5]
+
+// let newArr = []
+
+// const func = new Set(arr)
+
+// console.log(func);
+
+
+
+// const arr = [1,[2,[3,4],5]]
+
+// const newArr = arr.flat(Infinity);
+
+// console.log(newArr);
+
+
+ 
 
   return (
     <div>
